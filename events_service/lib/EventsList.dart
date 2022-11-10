526dea918000604eba4143936a57b11195589611
CalebDepatie <caleb.depatie@outlook.com>
2022-11-10 17:35:50 -0500

    Working attendance for tiles

--- events_service/lib/EventsList.dart
+++ events_service/lib/EventsList.dart
@@ -2,19 +2,53 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'EventTile.dart';
 import 'EventModel.dart';
+import 'UserModel.dart';
 
 class EventsList extends StatefulWidget {
   EventsList({super.key});
 
   final cloud = EventModel();
   late final Stream<QuerySnapshot> eventStream = cloud.getEvents();
+  final UserModel local_storage = UserModel();
+  List<String> attendingEvents = [];
 
   @override
   State<EventsList> createState() => _EventsListState();
 }
 
 class _EventsListState extends State<EventsList> {
-  Future getRecords() async {}
+  @override
+  void initState() {
+    widget.local_storage.connect().then((value) async {
+      widget.attendingEvents = await loadEvents();
+    });
+  }
+
+  Future<List<String>> loadEvents() async {
+    List<Map<String, Object?>> attend =
+        await widget.local_storage.getAttending();
+    return attend.map((row) => row["doc_id"] as String).toList();
+  }
+
+  Future<void> updateAttending(String evt_id) async {
+    if (await widget.local_storage.isAttending(evt_id)) {
+      await widget.local_storage.delAttendence(evt_id);
+    } else {
+      await widget.local_storage.attendEvent(evt_id);
+    }
+
+    SnackBar notif = const SnackBar(
+      content: Text("Attendance Updated"),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(notif);
+
+    List<String> newEvts = await loadEvents();
+
+    setState(() {
+      widget.attendingEvents = newEvts;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,18 +63,24 @@ class _EventsListState extends State<EventsList> {
                 (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
               if (!snapshot.hasData) return const Text("Loading...");
 
-              return Container(
-                child: ListView(
-                  children: snapshot.data!.docs.map((DocumentSnapshot doc) {
-                    Map<String, dynamic> data =
-                        doc.data()! as Map<String, dynamic>;
+              return FutureBuilder(
+                future: loadEvents(),
+                builder: (context, future_snapshot) => Container(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView(
+                    children: snapshot.data!.docs.map((DocumentSnapshot doc) {
+                      Map<String, dynamic> data =
+                          doc.data()! as Map<String, dynamic>;
 
-                    return EventTile(
-                      title: data["title"],
-                      location: data["location"],
-                      ref: doc.reference,
-                    );
-                  }).toList(),
+                      return EventTile(
+                        title: data["title"],
+                        location: data["location"],
+                        ref: doc.reference,
+                        attending: widget.attendingEvents.contains(doc.id),
+                        updateAttending: () => updateAttending(doc.id),
+                      );
+                    }).toList(),
+                  ),
                 ),
               );
             }));
