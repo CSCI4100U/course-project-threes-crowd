ca0f071baed4eee104179597552d5695c0e2d339
Yanguang-Yang <yanguang.yang@ontariotechu.net>
2022-12-06 14:52:14 -0500

    Add snackbar for no workable address

--- events_service/lib/EventView.dart
+++ events_service/lib/EventView.dart
@@ -63,7 +63,28 @@ class _EventViewState extends State<EventView> {
   }
 
   Future<void> geocode(String address) async {
-    final List<Location> locations = await locationFromAddress(address);
+    try{
+      final List<Location> locations = await locationFromAddress(address);
+        if(locations[0] != null){
+        current_loc = await Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.best);
+        send_loc = LatLng(locations[0].latitude, locations[0].longitude);
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => EventMap(
+                  loc: send_loc,
+                  current_loc: current_loc,
+                )));
+      }
+    }catch(e){
+      SnackBar snackBar = SnackBar(
+        content: Text('Please set a workable address'),
+      );
+      ScaffoldMessenger.of(context).removeCurrentSnackBar();
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+    
+    
+    /*
     current_loc = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.best);
     send_loc = LatLng(locations[0].latitude, locations[0].longitude);
@@ -71,7 +92,7 @@ class _EventViewState extends State<EventView> {
         builder: (context) => EventMap(
               loc: send_loc,
               current_loc: current_loc,
-            )));
+            )));*/
   }
 
   Future<void> onEdit(BuildContext context) async {
